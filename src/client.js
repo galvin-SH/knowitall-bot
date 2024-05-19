6651712fb6ed02f28316ed80a4aa10112c7fa6e6
galvin-SH <mgalvin1993@gmail.com>
2024-05-19 07:10:31 -0700

    added listeners for message creation and logic to reply to messages

--- src/client.js
+++ src/client.js
@@ -4,8 +4,11 @@ const {
 } = require('discord.js');
 
 module.exports = {
+    // Create a new client instance
     getClient() {
         return new Client({
+            // Enable the required gateway intents
+            // https://discord.com/developers/docs/topics/gateway#gateway-intents
             intents: [IntentsFlags.Guilds, IntentsFlags.GuildMessages],
         });
     },
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
 const client = require('./client').getClient();
+const { Events } = require('discord.js');
 
-client.on('ready', () => {
+// Listen for the ready event
+client.on(Events.ClientReady, () => {
     console.log(`Logged in as ${client.user.displayName}!`);
 });
 
+// Listen for the messageCreate event
+client.on(Events.MessageCreate, async (message) => {
+    // Ignore messages from bots and messages that don't mention the client
+    if (message.author.bot || !message.mentions.has(client.user)) return;
+    // Reply to the message
+    await message.reply('You mentioned me!');
+});
+
+// Define an async function to start the bot
 async function main() {
+    // Log in to Discord with the client
     await client.login(process.env.DISCORD_TOKEN);
 }
 
